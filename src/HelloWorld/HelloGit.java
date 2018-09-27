c8fbc7234133479671742076eb5524dd7cc3aff4
Daniel <danielpejcinovski@hotmail.com>
2018-09-27 22:59:00 +0200

    Print hello world three times.

--- src/HelloWorld/HelloGit.java
+++ src/HelloWorld/HelloGit.java
@@ -2,8 +2,10 @@ package HelloWorld;
 
 public class HelloGit {
 
-	void printHelloWorld(){
-		System.out.println("Hello world!\n");
+	static void printHelloWorld(){
+		System.out.println("Hello world!");
+		System.out.println("Hello world!");
+		System.out.println("Hello world!");
 	}
 	
 	public static void main(String[] args) {
