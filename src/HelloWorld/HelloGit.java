a826331c0c85e7e8bfcbb5e48f2aaedc2a068e57
Daniel <danielpejcinovski@hotmail.com>
2018-09-27 22:56:04 +0200

    Refactored hellogit.

--- src/HelloWorld/HelloGit.java
+++ src/HelloWorld/HelloGit.java
@@ -1,8 +1,13 @@
 package HelloWorld;
+
 public class HelloGit {
+
+	void printHelloWorld(){
+		System.out.println("Hello world!\n");
+	}
 	
 	public static void main(String[] args) {
-		// Refactor this
+		printHelloWorld();
 	}
 
 }
